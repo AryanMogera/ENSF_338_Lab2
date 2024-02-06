f43ae59fab4794a3cfc20499223ae41026c3944d
Aryanmogera <aryanmogaveera@gmail.com>
2024-02-05 20:34:55 -0700

    started 5

---
+++ ex5.py
@@ -0,0 +1,25 @@
+import timeit
+import random
+
+
+def linear_search(arr, target):
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return i
+  return -1
+
+def binary_search(arr, target):
+  low = 0
+  high = len(arr) - 1
+
+  while low <= high:
+    mid = (low + high) // 2
+    if arr[mid] == target:
+      return mid
+    elif arr[mid] < target:
+      low = mid + 1
+    else:
+      high = mid - 1 
+
+  return -1
+
