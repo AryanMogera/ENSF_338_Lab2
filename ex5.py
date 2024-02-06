17adc9bf8cc1b774fd04da936648eb56d5500170
Aryanmogera <aryanmogaveera@gmail.com>
2024-02-05 23:36:39 -0700

    changes

--- ex5.py
+++ ex5.py
@@ -53,29 +53,3 @@ for size in vector_sizes:
     print()
 
 
-from scipy.optimize import curve_fit
-
-def linear_func(x, a, b):
-    return a * x + b
-
-def quadratic_func(x, a, b, c):
-  return a * x**2 + b * x + c
-
-
-linear_params, _ = curve_fit(linear_func, vector_sizes, [average_linear_time for _ in vector_sizes])
-
-
-quadratic_params, _ = curve_fit(quadratic_func, vector_sizes, [average_linear_time for _ in vector_sizes])
-
-
-interpolated_linear = linear_func(vector_sizes, *linear_params)
-interpolated_quadratic = quadratic_func(vector_sizes, *quadratic_params)
-
-  
-plt.plot(vector_sizes, [average_linear_time for _ in vector_sizes], 'ro', label='Actual Data')
-plt.plot(vector_sizes, interpolated_linear, 'b-', label='Linear Interpolation')
-plt.plot(vector_sizes, interpolated_quadratic, 'g-', label='Quadratic Interpolation')
-plt.xlabel('Vector Size')
-plt.ylabel('Average Time (seconds)')
-plt.legend()
-plt.show()
